183db99029869458e68e1146847563fe68e09b65
ism47968960 <ism47968960@a02.informatica.escoladeltreball.org>
2018-02-15 13:52:08 +0100

    15-2-18

--- forA7.py
+++ forA7.py
@@ -4,10 +4,12 @@ print "SUMA ENTRE VALORES"
 
 num=int(input("Escriba un numero entero: "))
 num2=int(input("Escriba un numero entero mayor que "+str(num)+": "))
+aux=0
 
 while num2<=num:
 	print "Le he pedido un numero entero mayor que "+str(num)
 	num2=int(input("Escriba un numero entero mayor que "+str(num)+": "))
 	
 for i in range (num,num2+1):
-
+	aux+=i
+print "La suma desde",num,"hasta",num2,"es",aux
